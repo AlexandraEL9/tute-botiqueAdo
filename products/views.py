97b13c7e275d86417abc4ef27bc3dca2d00f6305
Alexandra Lee <alexandraelee9@gmail.com>
2024-10-22 14:12:39 +0000

    added sorting functionality- price and rating

--- products/views.py
+++ products/views.py
@@ -11,11 +11,28 @@ def all_products(request):
     products = Product.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
         if 'category' in request.GET:
+            # Split categories by comma, and filter products by category name
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
+            # Retrieve actual category objects for display purposes
             categories = Category.objects.filter(name__in=categories)
 
         if 'q' in request.GET:
@@ -24,16 +41,21 @@ def all_products(request):
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
             
+            # Filter by name or description based on the query
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting, 
     }
 
     return render(request, 'products/products.html', context)
+
 
 
 def product_detail(request, product_id):
