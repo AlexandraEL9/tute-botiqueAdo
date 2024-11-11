5266f0881796a0b3291a1043c2d047f8eb7d7862
Alexandra Lee <alexandraelee9@gmail.com>
2024-11-11 16:04:36 +0000

    add order summary to checkout success page

--- checkout/views.py
+++ checkout/views.py
@@ -87,7 +87,7 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': stripe_public_key,
+        'stripe_public_key': 'pk_test_51QI8onAS7iZraKTaGiCMEPaPNXV8czKkvfNYBb1VYvJnACK1LnTUNMImFuIcJ1GSWhvQ5r5OtCXKusquVaXMerKi00kRLscZzs',
         'client_secret': intent.client_secret,
     }
 
